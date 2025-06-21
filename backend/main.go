bc903c4f8729d7e9cadfc2b8432860c825ec4c3c
Aklantan <allan.tucker@gmail.com>
2025-06-21 20:44:04 +0100

    continuing adding the DB functions

--- backend/main.go
+++ backend/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"github.com/aklantan/foodcaptain/backend/handlers"
+	"github.com/aklantan/foodcaptain/backend/sql/queries"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	db := queries.InitDB()
 
 	server := gin.Default()
 	server.GET("/test", handlers.TestConnection)
