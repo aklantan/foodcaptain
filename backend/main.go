042683a229d29e5f9df06a476ee9d6ab3040be06
Aklantan <allan.tucker@gmail.com>
2025-06-21 21:44:47 +0100

    adding some basic frontend

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,27 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/aklantan/foodcaptain/backend/handlers"
 	"github.com/aklantan/foodcaptain/backend/models"
 	"github.com/aklantan/foodcaptain/backend/sql/queries"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"gorm.io/gorm"
 )
+
+type apiConfig struct {
+	db_query    *gorm.DB
+	tokenSecret string
+}
+
+func (c *apiConfig) returnRestaurants(g *gin.Context) {
+	var restaurants []models.Restaurant
+	c.db_query.Find(&restaurants)
+	g.JSON(http.StatusOK, gin.H{"restaraunts": restaurants})
+
+}
 
 func main() {
 
@@ -18,15 +31,18 @@ func main() {
 	}
 	db := queries.InitDB()
 
-	var restaurant []models.Restaurant
-	db.Find(&restaurant)
-
-	for _, res := range restaurant {
-		fmt.Println(res.RestaurantName)
+	apiCfg := &apiConfig{
+		db_query: db,
 	}
 
 	server := gin.Default()
+	server.Static("/static", "../frontend")
+	server.LoadHTMLFiles("../frontend/index.html")
+	server.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
 	server.GET("/test", handlers.TestConnection)
+	server.GET("/restaurants", apiCfg.returnRestaurants)
 
-	server.Run(":8999")
+	server.Run("127.0.0.1:8999")
 }
